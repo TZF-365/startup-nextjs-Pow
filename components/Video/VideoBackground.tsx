2fe34a65ccf4a336bd09d47ee34ec8d131db7512
Technologically Zealous Family <83666532+TZF-365@users.noreply.github.com>
2023-09-24 21:57:50 -0400

    Update VideoBackground.tsx

--- components/Video/VideoBackground.tsx
+++ components/Video/VideoBackground.tsx
@@ -1,11 +1,10 @@
- 
 // components/VideoBackground.tsx
 import React from 'react';
 
 const VideoBackground: React.FC = () => {
   return (
     <div className="video-background">
-      <video autoPlay loop muted playsInline>
+      <video autoPlay loop muted playsInline className="fixed top-0 left-0 w-full h-full z-[-1]">
         <source src="../video/Intro.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
