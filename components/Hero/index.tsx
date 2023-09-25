5959d7f55e3b4c51787f39d83cd4e18d3f1b8716
Technologically Zealous Family <83666532+TZF-365@users.noreply.github.com>
2023-09-24 21:46:38 -0400

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -10,8 +10,8 @@ const Hero = () => {
         className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
       >
         <div className="container">
-        <VideoBackground />
           <div className="-mx-4 flex flex-wrap">
+            <VideoBackground />
             <div className="w-full px-4">
               <div
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
